d5ccc6d05d041645a5db4b91b567a52bcf378d36
Mikhail Suharev <mishapsyxarev@gmail.com>
2023-09-13 10:32:38 +0800

    -add method.js

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
+import howDaysToNewYear from "./method.js";
 var now = new Date();
-var start = new Date(now.getFullYear(), 0, 0);
-var diff = now - start;
-var oneDay = 1000 * 60 * 60 * 24;
-var day = Math.floor(diff / oneDay);
-
-const numberOfDays = year => (((year % 4 === 0) && (year % 100 !== 0)) || (year % 400 === 0)) ? 366 : 365;
-
-console.log(numberOfDays(Date.getFullYear) - day);
+howDaysToNewYear(now);
